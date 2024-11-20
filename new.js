ff3f18e6bdfbef81ea4417ca0742d8add84206e7
Shelli <sh.panjieva@gmail.com>
2024-11-20 12:42:27 +0900

    new.js

---
+++ new.js
@@ -0,0 +1,9 @@
+let title = 'Shokhzoda';
+// console.log(title.length-1);
+let num = '12345654389'
+let start = title.slice(4,7);
+console.log(start)
+// let end = num.slice(-2)
+// console.log(end)
+// let res=start+'*****'+end;
+// console.log(res)
