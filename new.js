467ec1861b6b4b57a7e19321cdbcb698b745ff1e
Shelli <sh.panjieva@gmail.com>
2024-11-21 16:00:30 +0900

    k

--- new.js
+++ new.js
@@ -1,9 +1,16 @@
-let title = 'Shokhzoda';
-// console.log(title.length-1);
-let num = '12345654389'
-let start = title.slice(4,7);
-console.log(start)
+// let title = 'Shokhzoda';
+// console.log(title.length +''+ 'salom');
+// let num = '12345654389'
+// let start = title.slice(4,7);
+// console.log(start)
 // let end = num.slice(-2)
 // console.log(end)
 // let res=start+'*****'+end;
 // console.log(res)
+console.log(Math.ceil(Math.random(-10)));
+
+Math.min(100,20,50,-3,70,-100)
+console.log(
+    Math.min(100,20,50,-3,70,-100))
+    console.log(
+        Math.max(12000,20,50,-3,70,-100))
