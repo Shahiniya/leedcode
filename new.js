a54d6617258771cc8a4e3f8a35afadbe17882c47
Shelli <sh.panjieva@gmail.com>
2024-11-25 08:09:59 +0900

    d

--- new.js
+++ new.js
@@ -7,20 +7,32 @@
 // console.log(end)
 // let res=start+'*****'+end;
 // console.log(res)
-console.log(Math.ceil(Math.random(-10)));
+// console.log(Math.ceil(Math.random()*10));
 
-Math.min(100,20,50,-3,70,-100)
-console.log(
-    Math.min(100,20,50,-3,70,-100))
-    console.log(
-        Math.max(12000,20,50,-3,70,-100))
-     console.log(Math.ceil(Math.sqrt(96)))   
-     let son = 20;
-     console.log(son.toString())
-     let sona = 20.9856325;
-     console.log(sona.toFixed(2))
-     let r='2'
-     console.log(+r)
-     console.log(--r)
-     let d = new Date()
-     console.log(d.toUTCString())
+// Math.min(100,20,50,-3,70,-100)
+// console.log(
+//     Math.min(100,20,50,-3,70,-100))
+//     console.log(
+//         Math.max(12000,20,50,-3,70,-100))
+//      console.log(Math.ceil(Math.sqrt(96)))   
+//      let son = 20;
+//      console.log(son.toString())
+//      let sona = 20.9856325;
+//      console.log(sona.toFixed(2))
+//      let r='2'
+//      console.log(+r)
+//      console.log(--r)
+//      let d = new Date()
+//      console.log(d.toUTCString())
+     let cars = ['Saab','Volvo','BMW']
+    //  console.log(cars[1])
+    let car = cars[0]='malibu'
+
+    console.log(cars.toString())
+
+    let yoga = 'Salom,Hayr,boldi';
+    console.log(yoga.split())
+    let ar = ['tog','daraxt','tosh',12,5]
+    console.log(ar.join())
+    console.log(ar.push(5))
+    
