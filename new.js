8398f1c9201f9ca97494e82ca120cd0633bf503f
Shelli <sh.panjieva@gmail.com>
2024-11-26 06:40:27 +0900

    sal

--- new.js
+++ new.js
@@ -24,15 +24,83 @@
 //      console.log(--r)
 //      let d = new Date()
 //      console.log(d.toUTCString())
-     let cars = ['Saab','Volvo','BMW']
-    //  console.log(cars[1])
-    let car = cars[0]='malibu'
+    //  let cars = ['Saab','Volvo','BMW']
+    // //  console.log(cars[1])
+    // let car = cars[0]='malibu'
 
-    console.log(cars.toString())
+    // console.log(cars.toString())
 
-    let yoga = 'Salom,Hayr,boldi';
-    console.log(yoga.split())
-    let ar = ['tog','daraxt','tosh',12,5]
-    console.log(ar.join())
-    console.log(ar.push(5))
+    // let yoga = 'Salom,Hayr,boldi';
+    // console.log(yoga.split())
+    // let ar = ['tog','daraxt','tosh',12,5]
+    // console.log(ar.join())
+    // console.log(ar.push(5))
+    let x=7
+    // x++
+    x--
+    let y=1
+    let z=x
+    // console.log(!(x>y))
+    // console.log(x=1) 
+    // console.log(z) 
+    // let married = 'widowe'
+    // if(married === 'yes'){
+    //     console.log('you will accept')
+        
+    // }
+    // else if(married=='widowed'){
+    //     console.log('not accepted')
+        
+    // }
+    // else if(married ='engaged'){
+    //     console.log('you can apply after wedding')
+    // }
+    // else{console.log('Invalid answer');
+    // }
     
+
+
+
+    let abroad = 'ha'
+    let abroad1 = 0
+    switch(abroad){
+    case 'yes':
+    case 'ha': {
+      console.log(abroad1 = 'you do not accepted')
+      break;
+    }
+    case 'no':{
+       console.log(abroad1 = 'you can go')
+    }
+    default:{
+        console.log('wrong answer')
+    }
+}
+
+
+// node new.js
+// let par='salom1'
+// console.log(par.padEnd(8,'55'))
+
+let d=21/6
+console.log(d.toFixed(5))
+
+console.log(12+ +'12k')
+
+num = 23654123965874125n
+console.log(num * 2n/5n)
+
+let sonlar= [12,3,56,89,1,0,45]
+
+console.log((Math.min(...sonlar)))
+console.log((Math.min(12,5,6,3)))
+console.log((Math.min(+sonlar)))
+
+let g = -25.612563
+console.log(Math.abs(g))
+
+let grendeur = true 
+console.log(grendeur? 'Sotib olaman' : 'Boshqasini olishni oylab koraman' ) 
+
+let f = '123654'
+console.log(Number(f.length))
