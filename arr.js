e8e7c15e59748d941bbea67199eb12ddb33f77c1
Shelli <sh.panjieva@gmail.com>
2024-12-27 17:46:55 +0900

    o

--- arr.js
+++ arr.js
@@ -44,281 +44,290 @@ const renderData = (data) => {
 };
 // renderData(employees)
 
-const totalSalary=(data)=>{
-    let sum=0
+const totalSalary = (data) => {
+  let sum = 0;
 
-    data.forEach((item,index,arr)=>{
-        sum+=item.salary
-    })
-    console.log(`Total salary: $ ${sum}`)
-}
+  data.forEach((item, index, arr) => {
+    sum += item.salary;
+  });
+  console.log(`Total salary: $ ${sum}`);
+};
 // totalSalary(employees)
 
-const searchData = (data,searchText)=>{
-    const filterData=[]
+const searchData = (data, searchText) => {
+  const filterData = [];
 
-    data.forEach((item,index,arr)=>{
-        if(item.name.includes(searchText.split(' ')[0]) || 
-    item.surname.includes(searchText.split(' ')[1])){
-        filterData.push(item)
+  data.forEach((item, index, arr) => {
+    if (
+      item.name.includes(searchText.split(" ")[0]) ||
+      item.surname.includes(searchText.split(" ")[1])
+    ) {
+      filterData.push(item);
     }
-
-    })
-    renderData(filterData)
-}
+  });
+  renderData(filterData);
+};
 // searchData(employees, 'John Doe')
 
-const newUser={
-    name: "Mark",
-    surname: "Twen",
-    job: "Fronted Developer",
-    salary: 100000,
-}
+const newUser = {
+  name: "Mark",
+  surname: "Twen",
+  job: "Fronted Developer",
+  salary: 100000,
+};
 
-const addUser=(user)=>{
-    const newUser={
-        _id: String(employees.length+1),
-        ...user,
-    }
-    employees.push(newUser)
-}
+const addUser = (user) => {
+  const newUser = {
+    _id: String(employees.length + 1),
+    ...user,
+  };
+  employees.push(newUser);
+};
 // addUser(newUser)
 // renderData(employees)
 
-const updatedUser={
-    name: "Mark",
-    surname: "Twen",
-    job: "Fronted Developer",
-    salary: 100000,
-}
+const updatedUser = {
+  name: "Mark",
+  surname: "Twen",
+  job: "Fronted Developer",
+  salary: 100000,
+};
 
-
-const updateUser=(userId,body)=>{
-     const index = employees.findIndex(employees=>employees._id ===
-        userId
-     )
-     if(index===-1){
-        console.log('User not found');
-        return;
-     }
-     employees[index]={...employees[index],...body};
-     renderData(employees)
-}
+const updateUser = (userId, body) => {
+  const index = employees.findIndex((employees) => employees._id === userId);
+  if (index === -1) {
+    console.log("User not found");
+    return;
+  }
+  employees[index] = { ...employees[index], ...body };
+  renderData(employees);
+};
 // updateUser('2',updatedUser)
 
-const schoolDB={
-  g1:{
-    name:'G1 Frontend Bootcamp',
-    students:[
+const schoolDB = {
+  g1: {
+    name: "G1 Frontend Bootcamp",
+    students: [
       {
-        _id:'1',
-        name:'John Doe',
-        age:35,
-        grade:'1A'
+        _id: "1",
+        name: "John Doe",
+        age: 35,
+        grade: "1A",
       },
       {
-        _id:'1',
-        name: 'Michel Doe',
-        age:25,
-        grade:'2A'
+        _id: "1",
+        name: "Michel Doe",
+        age: 25,
+        grade: "2A",
       },
       {
-        _id:'1',
-        name:'Jane Doe',
-        age:22,
-        grade:'3A'
+        _id: "1",
+        name: "Jane Doe",
+        age: 22,
+        grade: "3A",
       },
-    ]
+    ],
   },
-  g2:{
-    name:'G2 Frontend Bootcamp',
-    students:[
+  g2: {
+    name: "G2 Frontend Bootcamp",
+    students: [
       {
-        _id:'1',
-        name:'John Doe',
-        age:11,
-        grade:'1A'
+        _id: "1",
+        name: "John Doe",
+        age: 11,
+        grade: "1A",
       },
       {
-        _id:'2',
-        name: 'Michel Doe',
-        age:15,
-        grade:'2A'
+        _id: "2",
+        name: "Michel Doe",
+        age: 15,
+        grade: "2A",
       },
       {
-        _id:'3',
-        name:'Jane Doe',
-        age:13,
-        grade:'3A'
+        _id: "3",
+        name: "Jane Doe",
+        age: 13,
+        grade: "3A",
       },
-    ]
+    ],
   },
-  g3:{
-    name:'G3 Frontend Bootcamp',
-    students:[
+  g3: {
+    name: "G3 Frontend Bootcamp",
+    students: [
       {
-        _id:'1',
-        name:'John Doe',
-        age:12,
-        grade:'1A'
+        _id: "1",
+        name: "John Doe",
+        age: 12,
+        grade: "1A",
       },
       {
-        _id:'2',
-        name: 'Michel Doe',
-        age:19,
-        grade:'2A'
+        _id: "2",
+        name: "Michel Doe",
+        age: 19,
+        grade: "2A",
       },
       {
-        _id:'3',
-        name:'Jane Doe',
-        age:21,
-        grade:'3A'
+        _id: "3",
+        name: "Jane Doe",
+        age: 21,
+        grade: "3A",
       },
-    ]
-  }
-}
+    ],
+  },
+};
 
-const renderGroupData = (group,groupname) => {
+const renderGroupData = (group, groupname) => {
   // console.log(`${groupname.name}\n`)
 
-         group.forEach((item,index,arr) => {
-      console.log(`${index+1}.${item.name} ${item.age} ${item.grade}`)
-    })
-}
+  group.forEach((item, index, arr) => {
+    console.log(`${index + 1}.${item.name} ${item.age} ${item.grade}`);
+  });
+};
 // renderGroupData(schoolDB.g1.students,schoolDB.g1)
 
-const updateGroupName = (groupName,newName) =>{
-    if(groupName.name !== newName){
-        groupName = newName
-    }
-    console.log(groupName)
-}
+const updateGroupName = (groupName, newName) => {
+  if (groupName.name !== newName) {
+    groupName = newName;
+  }
+  console.log(groupName);
+};
 // updateGroupName(schoolDB.g1,'G1 Frontend React.js Bootcamp')
 
-const newUsers={
-  _id:'10',
-  name:'Shokhzoda',
-  age:35,
-  grade:'5A'
-}
+const newUsers = {
+  _id: "10",
+  name: "Shokhzoda",
+  age: 35,
+  grade: "5A",
+};
 
 const addGroupUser = (data) => {
   const addData = {
-   _id:String(schoolDB.g2.students.length+1),
-   ...data
-  }
-  schoolDB.g2.students.push(addData)
-}
+    _id: String(schoolDB.g2.students.length + 1),
+    ...data,
+  };
+  schoolDB.g2.students.push(addData);
+};
 
 // addGroupUser(newUsers)
-// renderGroupData(schoolDB.g2.students) 
+// renderGroupData(schoolDB.g2.students)
 
-const schoolboysDB={
-  g1:{
-    name:'G1 Frontend Bootcamp',
-    students:[
+const schoolboysDB = {
+  g1: {
+    name: "G1 Frontend Bootcamp",
+    students: [
       {
-        _id:'1',
-        name:'John Doe',
-        age:35,
-        grade:'1A'
+        _id: "1",
+        name: "John Doe",
+        age: 35,
+        grade: "1A",
       },
       {
-        _id:'1',
-        name: 'Michel Doe',
-        age:25,
-        grade:'2A'
+        _id: "1",
+        name: "Michel Doe",
+        age: 25,
+        grade: "2A",
       },
       {
-        _id:'1',
-        name:'Jane Doe',
-        age:22,
-        grade:'3A'
+        _id: "1",
+        name: "Jane Doe",
+        age: 22,
+        grade: "3A",
       },
-    ]
+    ],
   },
-  g2:{
-    name:'G2 Frontend Bootcamp',
-    students:[
+  g2: {
+    name: "G2 Frontend Bootcamp",
+    students: [
       {
-        _id:'1',
-        name:'John Doe',
-        age:11,
-        grade:'1A'
+        _id: "1",
+        name: "John Doe",
+        age: 11,
+        grade: "1A",
       },
       {
-        _id:'2',
-        name: 'Michel Doe',
-        age:15,
-        grade:'2A'
+        _id: "2",
+        name: "Michel Doe",
+        age: 15,
+        grade: "2A",
       },
       {
-        _id:'3',
-        name:'Jane Doe',
-        age:13,
-        grade:'3A'
+        _id: "3",
+        name: "Jane Doe",
+        age: 13,
+        grade: "3A",
       },
-    ]
+    ],
   },
-  g3:{
-    name:'G3 Frontend Bootcamp',
-    students:[
+  g3: {
+    name: "G3 Frontend Bootcamp",
+    students: [
       {
-        _id:'1',
-        name:'John Doe',
-        age:12,
-        grade:'1A'
+        _id: "1",
+        name: "John Doe",
+        age: 12,
+        grade: "1A",
       },
       {
-        _id:'2',
-        name: 'Michel Doe',
-        age:19,
-        grade:'2A'
+        _id: "2",
+        name: "Michel Doe",
+        age: 19,
+        grade: "2A",
       },
       {
-        _id:'3',
-        name:'Jane Doe',
-        age:21,
-        grade:'3A'
+        _id: "3",
+        name: "Jane Doe",
+        age: 21,
+        grade: "3A",
       },
-    ]
-  }
-}
+    ],
+  },
+};
 
-const renderSchoolboysDB=(data)=>{
-      data.forEach((item,index,arr)=>{
-        console.log(`${index+1}. ${item.name}, ${item.age} ${item.grade}`)
-      })
-}
+const renderSchoolboysDB = (data) => {
+  data.forEach((item, index, arr) => {
+    console.log(`${index + 1}. ${item.name}, ${item.age} ${item.grade}`);
+  });
+};
 // renderSchoolboysDB(schoolboysDB.g1.students)
 
 const dataBase = {
-  _id:'10',
-        name:'Shokhzoda Panjizoda',
-        age:35,
-        grade:'5A'
-}
-const addSchoolboysDB=(data,newdata)=>{
-   data.forEach((item,index) => {
-        if(item!==newdata)
-        {}
-      })
-      console.log(data.push(newdata))
-// renderSchoolboysDB(schoolboysDB.g1.students)
-}
+  _id: "10",
+  name: "Shokhzoda Panjizoda",
+  age: 35,
+  grade: "5A",
+};
+const addSchoolboysDB = (data, newdata) => {
+  data.forEach((item, index) => {
+    if (item !== newdata) {
+    }
+  });
+  console.log(data.push(newdata));
+  // renderSchoolboysDB(schoolboysDB.g1.students)
+};
 // addSchoolboysDB(schoolboysDB.g1.students,dataBase)
 // renderSchoolboysDB(schoolboysDB.g1.students)
 
-const updateData=(data,newdata)=>{
-  const student= schoolboysDB.g2.students
-     const newlist = student.findIndex(student => student._id === data
-      )
-      if(newlist=== -1){
-        console.log('User not found')
-      }
-      student[newlist]={...student[newlist],...newdata}
-    //  employees[index]={...employees[index],...body};
-
-}
+const updateData = (data, newdata) => {
+  const student = schoolboysDB.g2.students;
+  const newlist = student.findIndex((student) => student._id === data);
+  if (newlist === -1) {
+    console.log("User not found");
+  }
+  student[newlist] = { ...student[newlist], ...newdata };
+  //  employees[index]={...employees[index],...body};
+};
 // updateData('2',dataBase)
 // renderSchoolboysDB(schoolboysDB.g2.students)
+
+const renderGroupDatas = (group) => {
+  if(group in schoolDB) {
+      console.log(`----- ${schoolDB[group].name} -----`)
+      schoolDB[group].students.forEach((student, index, array) => {
+          console.log(`Name: ${student.name}, Age: ${student.age}, Grade: ${student.grade}`)
+      })
+      console.log('------------------------------------')
+  } else {
+      console.log(`School has no ${group} group`)
+  }
+}
+renderGroupDatas('g2')
