a16782a12c300f08ee9abeda296a608f195319bb
Shelli <sh.panjieva@gmail.com>
2025-01-09 08:24:24 +0900

    o

--- arr.js
+++ arr.js
@@ -521,7 +521,7 @@ const addUsers = async (body) => {
 
 const wrapper = document.getElementById("wrapper");
 
-const renderUser = async () => {
+const renderUse = async () => {
   const users = await fetchUsers();
 
   if (users?.length > 0) {
@@ -542,4 +542,4 @@ const renderUser = async () => {
   }
 };
 
-renderUser();
+renderUse();
