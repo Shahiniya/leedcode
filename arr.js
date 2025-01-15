22d3b1f88662f24f7345466caaef3ad52cb855ac
Shelli <sh.panjieva@gmail.com>
2025-01-15 10:33:04 +0900

    o

--- arr.js
+++ arr.js
@@ -545,7 +545,7 @@ const renderUser = async () => {
 
 // renderUser();
 
-const renderProduct = async () => {
+const renderProductss = async () => {
   const products = await fetchUsers();
   
   if (products?.length > 0) {
@@ -577,4 +577,4 @@ const renderProduct = async () => {
   }
 };
 
-renderProduct()
+renderProductss()
